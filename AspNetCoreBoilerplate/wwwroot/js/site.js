90b09c27037ed64d4966f239bb91bb2365f7d2c4
Kristoffer Andreasen <kristofferaandreasen@gmail.com>
2017-06-25 22:41:28 +0200

    New modal

--- AspNetCoreBoilerplate/wwwroot/js/site.js
+++ AspNetCoreBoilerplate/wwwroot/js/site.js
@@ -36,7 +36,7 @@
         }
 
         // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
+        document.onclick = function(event) {
             if (event.target == modal) {
                 modal.style.display = "none";
             }
@@ -44,6 +44,7 @@
 
     }
     // End of Modal Function
+
 
     // -------------------------------------------------------------------------------------
     // Simple Modal Functions
@@ -53,33 +54,33 @@
     if (document.getElementById('mySimpleModal') != null) {
     
         // Get the modal
-        var modal = document.getElementById('mySimpleModal');
+        var simpleModal = document.getElementById('mySimpleModal');
 
         // Get the button that opens the modal
-        var btn = document.getElementById("myBtn");
+        var btn = document.getElementById("my_Simple_Modal_Btn");
         
         
         // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
+        var span = document.getElementsByClassName("simple_close")[0];
 
         // When the user clicks the button, open the modal 
         btn.onclick = function() {
-            modal.style.display = "block";
+            simpleModal.style.display = "block";
         }
 
         // When the user clicks on <span> (x), close the modal
         span.onclick = function() {
-            modal.style.display = "none";
+            simpleModal.style.display = "none";
         }
 
         // When the user clicks anywhere outside of the modal, close it
         window.onclick = function(event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
+            if (event.target == simpleModal) {
+                simpleModal.style.display = "none";
             }
         }
 
     }
-    // End of Simple Modal Function
+    // End of Modal Function
 
 });
